9d6328cfcd789fab359198fd84c4bb30d38922fe
gprohorov <yuramail222@gmail.com>
2020-03-10 18:15:32 +0200

    roles

--- src/main/java/pro/security/amg/security/ApplicationSecurityConfig.java
+++ src/main/java/pro/security/amg/security/ApplicationSecurityConfig.java
@@ -1,11 +1,16 @@
 package pro.security.amg.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 @Configuration
@@ -20,12 +25,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
                 authenticated().
                 and().
                 httpBasic();
-
-
-
-
     }
 
+/*    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails admin = User.builder().username("admin").password("admin")
+                .roles("ADMIN").build();
 
-
+        return new InMemoryUserDetailsManager(admin);
+    }*/
 }
