c322329af928128548a3e08a610339351f057b80
gprohorov <yuramail222@gmail.com>
2020-04-01 15:50:16 +0300

    workable

--- src/main/java/pro/security/amg/jwt/JwtConfig.java
+++ src/main/java/pro/security/amg/jwt/JwtConfig.java
@@ -1,4 +1,12 @@
 package pro.security.amg.jwt;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+
+
+
 public class JwtConfig {
+
+
 }
