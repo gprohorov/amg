d113ab0dcba2e51ecaf3e36071a0f56ab13eec3d
gprohorov <yuramail222@gmail.com>
2020-04-08 19:20:07 +0300

    WEB Config

--- src/main/java/pro/security/amg/security/ApplicationSecurityConfig.java
+++ src/main/java/pro/security/amg/security/ApplicationSecurityConfig.java
@@ -40,7 +40,7 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-   http.cors().disable();
+  // http.cors().disable();
         http
                .cors()
                 .and()
--- src/main/java/pro/security/amg/security/WebConfig.java
+++ src/main/java/pro/security/amg/security/WebConfig.java
@@ -1,16 +1,24 @@
 package pro.security.amg.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//@EnableWebMvc
 @Configuration
-@EnableWebMvc
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig{
+        //implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**");
+            }
+        };
     }
 }
